451a8fd400aac106d31ac876698fff89d57c9214
prasanth-sim <prasanth.reddy@simadvisorytech.com>
2025-04-11 02:55:10 -0700

    testjava

---
+++ test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    // A simple method that adds two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(5, 10);
+        System.out.println("The result of adding 5 and 10 is: " + result);
+    }
+}
